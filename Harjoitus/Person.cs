6c8830a6e2fb3be5abc578e5dc6bc0f49dbe0d38
Jaakki <62286646+Jaakki@users.noreply.github.com>
2020-03-30 13:53:50 +0300

    Add project files.

---
+++ Harjoitus/Person.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Harjoitus
+{
+    class Person
+    {
+        public string name;
+        public int age;
+        public int salary;
+        public Person friend;
+
+        public Person(string name, int age)
+        {
+            this.name = name;
+            this.age = age;
+        }
+
+        public Person()
+        {
+
+        }
+    }
+}
---
+++ Harjoitus/Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Harjoitus
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person per1 = new Person();
+            per1.name = "Seppo";
+            per1.age = 29;
+            per1.salary = 500;
+
+            Person per2 = new Person();
+            per2.name = "Titta";
+            per2.age = 26;
+            per2.salary = 450;
+
+            Person per3 = new Person();
+            per3.name = "Sauli";
+            per3.age = 60;
+            per3.salary = 5000;
+
+            Console.WriteLine($"{per1.name} {per1.age} {per1.salary}");
+            Console.WriteLine($"{per2.name} {per2.age} {per2.salary}");
+            Console.WriteLine($"{per3.name} {per3.age} {per3.salary}");
+            Console.WriteLine();
+
+            per1.name = "Teppo";
+            per1.age = 32;
+            per1.salary = 800;
+
+            per3.name = "Kuningas Sauli";
+            per3.age = 70;
+            per3.salary = 10000;
+
+            Console.WriteLine($"{per1.name} {per1.age} {per1.salary}");
+            Console.WriteLine($"{per2.name} {per2.age} {per2.salary}");
+            Console.WriteLine($"{per3.name} {per3.age} {per3.salary}");
+            Console.WriteLine();
+
+            Person per4 = new Person();
+            per4.name = "Pentti";
+            per4.age = 13;
+
+            Person per5 = new Person();
+            per5.name = "Juuso";
+            per5.age = 33;
+
+            Console.WriteLine($"{per4.name} {per4.age}");
+            Console.WriteLine($"{per5.name} {per5.age}");
+            Console.WriteLine();
+
+            Person juuso = new Person();
+            juuso.name = "Juuso";
+
+            Person pekka = new Person();
+            pekka.name = "Pekka";
+            pekka.friend = juuso;
+
+            Person antti = new Person();
+            antti.name = "Antti";
+            antti.friend = pekka;
+
+            Console.WriteLine($"{pekka.name} kaveri on {pekka.friend.name}. {antti.name} kaveri on {antti.friend.name}.");
+            Console.WriteLine();
+
+            List<string> Persons = new List<string>();
+            Persons.Add(per1.name);
+            Persons.Add(per2.name);
+            Persons.Add(per3.name);
+            Persons.Add(per4.name);
+            Persons.Add(per5.name);
+
+            List<int> Ages = new List<int>();
+            Ages.Add(per1.age);
+            Ages.Add(per2.age);
+            Ages.Add(per3.age);
+            Ages.Add(per4.age);
+            Ages.Add(per5.age);
+
+            foreach (var name in Persons)
+            {
+                Console.WriteLine(name);
+            }
+
+            int addedAges = 0;
+            foreach (var age in Ages)
+            {
+                addedAges = addedAges + age;
+                Console.WriteLine($"Ikä: {age}");
+            }
+
+            Console.WriteLine($"Yhteenlaskettu ikä: {addedAges}");
+
+        }
+    }
+}
